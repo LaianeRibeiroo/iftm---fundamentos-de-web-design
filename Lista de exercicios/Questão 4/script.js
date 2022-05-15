86a2ca69d1659f59f33a5fbc0a995ad54ae6fad0
laianeiftm <laiane.ribeiro@sankhya.com.br>
2022-05-15 18:08:11 -0300

    Atualizando conteudos

---
+++ Lista de exercicios/Questão 4/script.js
@@ -0,0 +1,26 @@
+function mouseEnter() {
+    document.getElementById("foto1").src = "imagens/batman10g.png";
+  }
+
+  function mouseLeave() {
+    document.getElementById("foto1").src = "bat";
+  }
+
+  function mouseEnter() {
+    document.getElementById("foto2").src="imagens/wonderwoman8g.png";
+  }
+
+  function mouseLeave() {
+    document.getElementById("foto2").src = "won";
+  }
+
+  function mouseEnter() {
+    document.getElementById("foto3").src="imagens/superman2g.png" ;
+  }
+
+  function mouseLeave() {
+    document.getElementById("foto3").src = "sup";
+  }
+
+
+  
