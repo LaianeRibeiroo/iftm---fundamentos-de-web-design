e7260c2e6ed1adab2c50ebc0294b55b390ae198d
laianeiftm <laiane.ribeiro@sankhya.com.br>
2022-05-18 21:42:26 -0300

    Atualizando repositório - Entrega da atividade

--- Lista de exercicios/Questão 4/script.js
+++ Lista de exercicios/Questão 4/script.js
@@ -1,26 +1,28 @@
-function mouseEnter() {
+function mouseEnter1() {
     document.getElementById("foto1").src = "imagens/batman10g.png";
   }
 
-  function mouseLeave() {
-    document.getElementById("foto1").src = "bat";
-  }
+  function mouseLeave1() {
+    document.getElementById("foto1").src = "imagens/batman10.png";
+  }//aqui tem que ter a imagem pro qual ela vai retornar!
 
-  function mouseEnter() {
+  function mouseEnter2() {
     document.getElementById("foto2").src="imagens/wonderwoman8g.png";
   }
 
-  function mouseLeave() {
-    document.getElementById("foto2").src = "won";
+  function mouseLeave2() {
+    document.getElementById("foto2").src = "imagens/wonderwoman8.png";
   }
 
-  function mouseEnter() {
+  function mouseEnter3() {
     document.getElementById("foto3").src="imagens/superman2g.png" ;
   }
 
-  function mouseLeave() {
-    document.getElementById("foto3").src = "sup";
+  function mouseLeave3() {
+    document.getElementById("foto3").src = "imagens/superman2.png";
   }
+//o nome de cada função TEM QUE SER DIFERENTE!
+//como vc estava usando tudo com mesmo nome, só o super
+//estava funcionando pois era a última função!
 
-
-  
+//com essas alterações que eu fiz, agora é so estilizar o css!
