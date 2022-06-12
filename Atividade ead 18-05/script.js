6b3245c2dca0b61fe625b4379d6392523abd19f9
laianeiftm <laiane.ribeiro@sankhya.com.br>
2022-06-11 22:05:16 -0300

    Atualizando repositório

--- Atividade ead 18-05/script.js
+++
@@ -1,5 +0,0 @@
-function trocanoticia(num){
-    document.getElementById("fotof").src="fotos/f" + num + ".jpg";
-    document.getElementById("titulot").src="titulos/t" + num + ".html";
-    document.getElementById("notician").src="noticias/n" + num + ".html";
-    }
